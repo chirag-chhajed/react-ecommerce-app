9ca56578d4a116c1181fd34b50a17c8f2f9d3342
Chirag Chhajed <chhajedchirag18@gmail.com>
2022-11-07 22:20:44 +0530

    confetti effect added

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,11 @@
 import React, { useContext, useState } from "react";
 import CartItem from "../Components/CartItem";
 import { Context } from "../Context";
+import Confetti from 'react-confetti'
 
 export default function Cart() {
     const [buttonText, setButtonText] = useState("Place Order")
+    const [confetti,useConfetti] = useState(false)
     const { cartItems, emptyCart } = useContext(Context)
     const cartProducts = cartItems.map(e => (
         <CartItem
@@ -11,15 +13,19 @@ export default function Cart() {
             {...e}
         />
     ))
+    // const priceArray = cartItems.map(e => e.price)
+    const initialValue = 0
     const totalprice = cartItems.map(e => e.price).reduce(
-        (a, b) => parseInt(a) + parseInt(b)
+        (a, b) => parseInt(a) + parseInt(b),initialValue
     )
     function placeOrder() {
+        useConfetti(true)
         setButtonText("Ordering")
         setTimeout(() => {
             setButtonText("Place Order")
             emptyCart()
-        }, 4000)
+            useConfetti(false)
+        }, 5000)
     }
     // const display = cartItems.length > 0 
     //                     ? ` ${cartProducts}`
@@ -30,7 +36,7 @@ export default function Cart() {
             {
                 cartItems.length === 0
                 &&
-                <h2>You have no items in your cart <br />😞😞😞</h2>
+                <h2 className="text-center text-8xl">You have no items in your cart <br />😞😞😞</h2>
             }
             {
                 cartItems.length > 0
@@ -49,7 +55,8 @@ export default function Cart() {
                 cartItems.length > 0
                 &&
                 <>
-                    <div className="text-right mx-8 font-bold">Total Price: Rs.{totalprice}</div>
+                    {confetti && <Confetti/>}
+                    <div className="text-right mx-8 font-bold">Total Price: Rs. {totalprice}</div>
                     <div className="text-center my-8">
                         <button onClick={placeOrder}>
                             <span class="button_top">
